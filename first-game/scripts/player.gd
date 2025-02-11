120f06032bc1685161df36d0c8b8de52a53f90b5
rudraa19 <rudra19206@gmail.com>
2025-02-11 21:09:51 +0530

    final commit before export 1.0

--- first-game/scripts/player.gd
+++ first-game/scripts/player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
-
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
-
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
