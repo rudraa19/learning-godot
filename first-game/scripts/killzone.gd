6fd90f850a644215fc13e39e42eade0d942530df
rudraa19 <rudra19206@gmail.com>
2025-02-11 20:30:57 +0530

    add sound

--- first-game/scripts/killzone.gd
+++ first-game/scripts/killzone.gd
@@ -1,10 +1,12 @@
 extends Area2D
 
 @onready var timer = $Timer
+@onready var audio_stream_player_2d = $AudioStreamPlayer2D
 
 func _on_body_entered(body):
 	Engine.time_scale = 0.5
 	body.get_node("CollisionShape2D").queue_free()
+	audio_stream_player_2d.play()
 	timer.start()
 
 func _on_timer_timeout():
