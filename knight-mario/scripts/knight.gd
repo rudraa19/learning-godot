a46135893b53a282ddf4e67e16c4fa123468144a
rudraa19 <rudra19206@gmail.com>
2025-04-20 18:27:59 +0530

    chore: add basic player movement

--- knight-mario/scripts/knight.gd
+++ knight-mario/scripts/knight.gd
@@ -1,4 +1,10 @@
 extends CharacterBody2D
 
 func _physics_process(delta):
-	print("hi")
+	move_and_slide()
+	
+	if Input.is_action_pressed("right"):
+		velocity.x = 100
+	if Input.is_action_pressed("left"):
+		velocity.x = -100
+	velocity.x = lerp(velocity.x, 0.0, 0.2)
