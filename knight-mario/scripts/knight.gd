36c88bbd48cc5a5e0bb4cd6eee7e6d694371241a
rudraa19 <rudra19206@gmail.com>
2025-04-24 20:04:30 +0530

    chore: add animations

--- knight-mario/scripts/knight.gd
+++ knight-mario/scripts/knight.gd
@@ -8,8 +8,17 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.play("idle")
+	
+	if not is_on_floor():
+		$AnimatedSprite2D.play("air")
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_FORCE
